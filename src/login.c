cb7c88b9b4bdfad70b8135067a9f05a9e1663e64
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-05 01:48:02 +0800

    added parse_db()

--- src/login.c
+++ src/login.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #define MAXLEN 1000
 
-typedef struct {
+typedef struct usr_node {
     char username[MAXLEN];
     char password[MAXLEN];
 } User;
 
 // Parses /DB and creates a linked list of User structs
 User* parse_db() {
-    
+    User *tmp;
+    FILE *ifp;
+    int num_of_users;
+
+    ifp = fopen("count.txt", "rt");
+    fscanf(ifp, "%d", &num_of_users);
+    fclose(ifp);
+
+    for (int i = 1; i <= num_of_users; i++) {
+        ifp = fopen(("%d.txt", i), "rt");
+
+        tmp = (User *) malloc(sizeof(User));
+        fscanf(ifp, "%s", tmp->username);
+        fscanf(ifp, "%s", tmp->password);
+    }
+
     return;
 }
