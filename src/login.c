c0c938a949f1ca6be87143384f51b6aa8f03b372
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-05 01:22:26 +0800

    added a DB entry
    moved account_management.c code to login.c

---
+++ src/login.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAXLEN 1000
+
+typedef struct {
+    char username[MAXLEN];
+    char password[MAXLEN];
+} User;
+
+// Parses /DB and creates a linked list of User structs
+User* parse_db() {
+    
+    return;
+}
