780975df9e367df1bf108cbe37934feff0ca5472
makiroll.mark <delacruzmacky39@gmail.com>
2024-04-04 14:44:34 +0800

    added the headers

--- MBP.c
+++ MBP.c
@@ -1,10 +1,34 @@
+//headers
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 10000
 
-int main() {
+
+typedef struct 
+{
+    int id;
+    char food_name[100];
+    float price;
+    float rating;
+    float ratio;
+}food_item;
+
+
+int main (){
+    int i;
+    FILE *ifp, *ofp;
+
+
+
+
+    printf("What would you like to do? \n");
 	printf("Hello World\n");
 	return 0;
 	//hiiiiiiiiiii
+    
 }
+
 
 // hi mark
 //hello JP
