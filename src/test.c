7c9c62bf3f4cf158092b2161e444ff09cf1ded7b
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-06 18:50:25 +0800

    added test for createAccount

--- src/test.c
+++ src/test.c
@@ -3,13 +3,16 @@
 #include "login.h"
 
 int main() {
-    User *user;
+    User *user_list;
+    User *curr_user;
 
-    user = parse_db();
-    user = user->next;
+    user_list = parse_db();
+    curr_user = user_list->next; // skip head
 
-    printf("%s\n", user->username);
-    printf("%s", user->password);
+    printf("%s\n", curr_user->username);
+    printf("%s", curr_user->password);
+
+    createAccount("Jelyn", "1234", user_list);
 
     return 0;
 }
