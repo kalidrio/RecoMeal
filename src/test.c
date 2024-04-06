819af5b3e73f8a3daee7fc03d7981854a6393bca
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-06 20:43:48 +0800

    okay so now this works after the memory leak was fixed???

--- src/test.c
+++ src/test.c
@@ -6,13 +6,18 @@ int main() {
     User *user_list;
     User *curr_user;
 
-    user_list = parse_db();
+    user_list = parseDB();
     curr_user = user_list->next; // skip head
 
     printf("%s\n", curr_user->username);
-    printf("%s", curr_user->password);
+    printf("%s\n", curr_user->password);
 
     createAccount("Jelyn", "1234", user_list);
+
+    curr_user = curr_user->next; // next user
+
+    printf("%s\n", curr_user->username);
+    printf("%s", curr_user->password);
 
     return 0;
 }
