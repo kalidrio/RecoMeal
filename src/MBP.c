bf2545064b1bcb16810749c452e72c66b6f34730
makiroll.mark <delacruzmacky39@gmail.com>
2024-04-12 15:15:41 +0800

    Update MBP.c
    suggest mode

--- src/MBP.c
+++ src/MBP.c
@@ -1,20 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#define MAX 10000
+typedef struct 
+{
+    int id;
+    char food_name[100];
+    float price;
+    float rating;
+    float sulit_value;
+}food_item;
 
-void print_Menu(void) {
-	printf("\nWhat would you like to do? [ENTER THE NUMBER]\n");
-	printf("\t1. Peruse Mode\n");	
-	printf("\t2. Suggest Mode\n");
-	printf("\t3. Input Mode\n");
-	printf("\t4. Exit\n");
-	return;
-}
 
-int S_init(void) {
+void print_menu(void);
+//void peruse_mode(FILE *fp);
+void suggest_mode(FILE *fp, FILE *fp1);
+//void input_mode(FILE *fp);
+
+
+
+
+int main(void) {
 	int option = 0;
 	char extra;  // dummy var to catch trailing input
 	while (option != 4) {
+	FILE *fp;
+	FILE *fp1;
 
-		print_Menu();
+
+	fp = fopen("../DB/food_items.txt", "r+");  //opens the file for reading and writing
+	fp1 = fopen("../DB/purchase_history.txt", "r+");
+		print_menu();
 
 		// extra catches trailing non-numeric characters
 		if (scanf("%d%c", &option, &extra) != 2 || extra != '\n')
@@ -26,10 +41,10 @@ int S_init(void) {
 		else {
 			switch(option){
 				case 1:
-					//peruse mode
+					//peruse_mode(fp);
 					break;
 				case 2:
-					//display_mode(fp);
+					suggest_mode(fp, fp1);
 					break;
 				case 3:
 					//input_mode(fp);
@@ -46,6 +61,38 @@ int S_init(void) {
 }	
 
 
+void print_menu(void) {
+	printf("\nWhat would you like to do? [ENTER THE NUMBER]\n");
+	printf("\t1. Peruse Mode\n");	
+	printf("\t2. Suggest Mode\n");
+	printf("\t3. Input Mode\n");
+	printf("\t4. Exit\n");
+	return;
+}
+
+food_item* file_reading(FILE *file) 
+{
+    food_item* temp = (food_item*)malloc(MAX * sizeof(food_item));
+    int i = 0;
+    while (fscanf(file, "%d %99s %f %f %f", &temp[i].id, temp[i].food_name, &temp[i].price, &temp[i].rating, &temp[i].sulit_value) == 5) {
+        i++;
+    }
+    return temp;
+}
 
 
+
+void suggest_mode(FILE *fp, FILE *fp1){
+	//fp1 is the last 10 transactions
+	food_item data[MAX];
+	//will just be using pointers and not using structures every time.
+	food_item* history_ptr = file_reading(fp1);
+	food_item* data_ptr = file_reading(fp);
+	printf("These are your last 10 purchases: \n");
+    for (int i = 0; i < 10; i++){
+        printf("ID: %d  Food: %s  Rating: %f\n", history_ptr[i].id, history_ptr[i].food_name, history_ptr[i].rating);
+    }
+   //freeing the memory allocated by file_reading
+    free(history_ptr);
+}
 
