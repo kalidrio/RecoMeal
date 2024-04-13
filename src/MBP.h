5019c2fbddb1862d6bfd5f30735773871b5f420c
Kalidrio <jpigd2023-4376-87684@bicol-u.edu.ph>
2024-04-13 10:26:26 +0800

    added documentation to header file

--- src/MBP.h
+++ src/MBP.h
@@ -6,11 +6,13 @@ typedef struct {
         float sulit_value;
 } food_item;
 
+// user defined functions
 void read_data(FILE *from_catalogue, food_item* catalogue);	
-
 void print_Menu(void);
 
+// case-dependent functions
 void peruse_mode(FILE* from_catalogue, food_item* catalogue);
 
+// initialiazation functions
 int S_init(void);
 
