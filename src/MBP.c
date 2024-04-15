81bac8dea0f6ff9e4e6556027c3eef9ed6bf86e0
makiroll.mark <delacruzmacky39@gmail.com>
2024-04-15 15:36:26 +0800

    Update MBP.c

--- src/MBP.c
+++ src/MBP.c
@@ -12,6 +12,7 @@ typedef struct
 
 
 void print_menu(void);
+void print_menu2(void);
 //void peruse_mode(FILE *fp);
 void suggest_mode(FILE *fp, FILE *fp1);
 //void input_mode(FILE *fp);
@@ -22,17 +23,17 @@ void suggest_mode(FILE *fp, FILE *fp1);
 int S_init(void) {
 	int option = 0;
 	char extra;  // dummy var to catch trailing input
-	while (option != 4) {
 	FILE *fp;
 	FILE *fp1;
 
 
 	fp = fopen("../DB/food_items.txt", "r+");  //opens the file for reading and writing
 	fp1 = fopen("../DB/purchase_history.txt", "r+");
+	
+	while (option != 4) {
 		print_menu();
-
 		// extra catches trailing non-numeric characters
-		if (scanf("%d%c", &option, &extra) != 2 || extra != '\n')
+		if (scanf("%d%c", &option, &extra) != 2 && extra != '\n')
 	       	{
 			printf("\nProgram exited with error code [01]:\n");
 			printf("Input was not a single digit integer.\n");      
@@ -69,6 +70,14 @@ void print_menu(void) {
 	printf("\t4. Exit\n");
 	return;
 }
+void print_menu2(void) {
+	printf("\nFilter the food items based on: [ENTER THE NUMBER]\n");
+	printf("\t1. Budget\n");	
+	printf("\t2. Ratings\n");
+	printf("\t3. Sulit Value\n");
+	printf("\t4. Return to Home.\n");
+	return;
+}
 
 food_item* file_reading(FILE *file) 
 {
@@ -89,10 +98,37 @@ void suggest_mode(FILE *fp, FILE *fp1){
 	food_item* history_ptr = file_reading(fp1);
 	food_item* data_ptr = file_reading(fp);
 	printf("These are your last 10 purchases: \n");
-    for (int i = 0; i < 10; i++){
+    
+	for (int i = 0; i < 10; i++){
         printf("ID: %d  Food: %s  Rating: %f\n", history_ptr[i].id, history_ptr[i].food_name, history_ptr[i].rating);
+    	}
+   	
+    while (1) {
+        print_menu2();
+        if (scanf("%d%c", &option, &extra) != 2 || extra != '\n') {
+            printf("\nProgram exited with error code [01]:\n");
+            printf("Input was not a single digit integer.\n");      
+            return 1;
+        } else {
+            switch(option) {
+                case 1:
+                    printf("try");	//budget mode			
+                    break;
+                case 2:
+                    printf("try again"); //rating mode
+                    break;
+                case 3:
+                    printf("try until you succeed"); //sulit mode
+                    break;
+                case 4:
+                    printf("Returning to Home...\n"); //exit
+					free(history_ptr); //freeing the memory allocated by file_reading
+                    return;
+                default:
+                    printf("Invalid option. Please choose again.\n");
+            }
+        }
     }
-   //freeing the memory allocated by file_reading
-    free(history_ptr);
+}
 }
 
