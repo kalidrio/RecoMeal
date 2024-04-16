74bf193ef6f23fe3678dddb868fc69ac1ea154cf
Kalidrio <jpigd2023-4376-87684@bicol-u.edu.ph>
2024-04-16 11:45:24 +0800

    Error handling FINNALLY finished & working
    need to accomodate dynamic file size/input

--- src/MBP.c
+++ src/MBP.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "MBP.h"
 
 void read_data(FILE* from_catalogue, food_item* catalogue) {
 	int i = 0;
-	while (fscanf(from_catalogue, "%d %s %f %f %f",
+	int result;
+	while ((result = fscanf(from_catalogue, "%d %s %f %f %f",
 				      &catalogue[i].ID,
 				      catalogue[i].name,
 				      &catalogue[i].price,
 				      &catalogue[i].rating,
-				      &catalogue[i].sulit_value) == 5) {
+				      &catalogue[i].sulit_value)) == 5) {
         	i++;
+	}
+	if (result != EOF && result != 5) { // error handling
+		fprintf(stderr, "Error reading data.\n");
+	}
+
+	if (ferror(from_catalogue)) { // more detailed error msg
+		perror("Error: \n");
 	}
 }
 
@@ -23,29 +33,49 @@ void print_Menu(void) {
 	printf("\nChoice: ");
 }
 
-void peruse(FILE* from_catalogue, food_item* catalogue) {
+void peruse(FILE* from_catalogue, food_item* catalogue, int* errPtr) {
 	for (int i = 0; i < 13; i++) {
 		printf("\t%03d   %s\n", catalogue[i].ID, catalogue[i].name);
 	}
 
 	int option;
-	while (1) {
-		printf("\nInput ID# of Item for more details or 0 to exit: ");
-		if (scanf("%d", &option) != 1 || (option > 13 || option < 0) || option == 0) {
-		printf("Please enter a valid number.\n");
-		break;
-	}
-	else {
+	char trail; // dummy var to catch trailing characters
+	int result;
+
+	printf("\nEnter ID# for more info or 0 to exit: ");
+	while ((result = scanf("%d%c", &option, &trail)) != EOF) {
+		if (result != 2 || trail != '\n') {
+			printf("\nProgram exited with error: ");
+			printf("\n\tInput was not solely an integer\n");
+			*errPtr = 1;
+			return;
+		}
+
+		if (option < 0 || option > 13) { 
+			printf("\nPlease enter an ID#(1 - 13): ");
+			continue;
+
+		} else 	if (option == 0) return;
+
 		printf("\n Dish Name: %s\n", catalogue[option-1].name);
 		printf("     Price: Php %.2f\n", catalogue[option-1].price);
 		printf("    Rating: %.2f/10\n", catalogue[option-1].rating);
 		printf("Sulit-ness: %.2f\n", catalogue[option-1].sulit_value);
-		}
+
+		printf("\nEnter ID# for more info or 0 to exit: ");
 	}
+
+	printf("Ctrl-D(Program end).\n");
+	*errPtr = 1;
+	return;
+
 }
 
 int S_init(void) {
 	int option = 0;
+	char trail; // dummy var to catch trailing characters
+	int error = 0;
+	int* errPtr = &error;
 
 	FILE* from_catalogue = fopen("../DB/catalogue.txt", "r");
 	if (!from_catalogue) {
@@ -61,10 +91,13 @@ int S_init(void) {
 
 	read_data(from_catalogue, catalogue);
 
-	while (option != 4) {
-		print_Menu();
-		if (scanf("%d", &option) != 1) {
-			printf("\nProgram exited with error: Input was not a single digit integer.\n");
+	while (option != 4 && !error) {
+		if (!error) {
+			print_Menu();
+		}
+		if (scanf("%d%c", &option, &trail) != 2 || trail != '\n') {
+			printf("\nProgram exited with error: ");
+			printf("\n\tInput was not solely an integer.\n");
 			fclose(from_catalogue);
 			free(catalogue);
 			return 1;
@@ -73,7 +106,7 @@ int S_init(void) {
 			printf("\n");
 			switch(option){
 				case 1:
-					peruse(from_catalogue, catalogue);
+					peruse(from_catalogue, catalogue, errPtr);
 					break;
 				case 2:
 					// Implement Suggest Mode
--- src/MBP.h
+++ src/MBP.h
@@ -9,7 +9,7 @@ typedef struct {
 // User defined functions
 void read_data(FILE *from_catalogue, food_item* catalogue);
 void print_Menu(void);
-void peruse(FILE* from_catalogue, food_item* catalogue);
+void peruse(FILE* from_catalogue, food_item* catalogue, int* errPtr);
 
 // Initialization functions
 int S_init(void);
