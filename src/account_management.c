2f66c5f9ccbaff8373dc2e17d368d3d07e9dc3c1
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-17 12:17:47 +0800

    added function headers account_management.c/h

---
+++ src/account_management.c
@@ -0,0 +1,14 @@
+#include "account_management.h"
+#include "structs.h"
+
+void changeUser(User account) {
+
+}
+
+void changePass(User account) {
+
+}
+
+void deleteAccount(User account) {
+
+}
---
+++ src/account_management.h
@@ -0,0 +1,5 @@
+void changeUser(User account);
+
+void changePass(User account);
+
+void deleteAccount(User account);
