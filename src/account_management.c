1c623f9619ac760118cae3737dc093610fb95681
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-17 12:46:09 +0800

    small addition

--- src/account_management.c
+++ src/account_management.c
@@ -2,7 +2,14 @@
 #include "structs.h"
 
 void changeUser(User account) {
+    char new_username[MAXLEN];
 
+    // Input new username
+    printf("Input new username: ");
+    scanf("%s", new_username);
+
+    // Save to structure
+    
 }
 
 void changePass(User account) {
