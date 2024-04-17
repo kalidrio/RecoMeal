0ca6e380c973da020e8b47dbf5305269c525374b
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-17 13:15:49 +0800

    refactor of parseDB and createAccount, with test.c

--- src/login.c
+++ src/login.c
@@ -14,21 +14,25 @@ User* parseDB() {
 
     User *tmp;
     FILE *ifp;
-    char curr_filename[MAXLEN];
-    int num_of_users;
+    char username[MAXLEN];
+    char password[MAXLEN];
+    int counter = 1;
 
-    ifp = fopen("DB/count.txt", "rt");
-    fscanf(ifp, "%d", &num_of_users);
-    fclose(ifp);
+    ifp = fopen("DB/accounts.txt", "rt");
 
     head = (User *) malloc(sizeof(User));
 
-    for (int i = 1; i <= num_of_users; i++) {
-        sprintf(curr_filename, "DB/%d.txt", i);
-        ifp = fopen(curr_filename, "rt");
+    while (1) {
+        // Get username and password
+        if (fscanf(ifp, "%s", username) != EOF) {
+            fscanf(ifp, "%s", password);
+        }
+        else {
+            break; // Exit on EOF
+        }
 
         // Link to head if first user
-        if (i == 1) {
+        if (counter == 1) {
             head->next = (User *) malloc(sizeof(User));
             tmp = head->next;
         }
@@ -39,8 +43,10 @@ User* parseDB() {
         }
 
         // Create User struct for current user
-        fscanf(ifp, "%s", tmp->username);
-        fscanf(ifp, "%s", tmp->password);
+        strcpy(tmp->username, username);
+        strcpy(tmp->password, password);
+
+        counter++;
     }
 
     // Set last user's next pointer to NULL
@@ -118,24 +124,11 @@ void createAccount(char username[], char password[], User* user_list) {
 // Saves new user accounts to the DB
 // DO NOT CALL THIS FUNCTION, call createAccount instead
 void saveAccountToDB(User account) {
-    FILE *count_file;
-    FILE *new_user_file;
-    int num_of_users;
-    char filename[MAXLEN];
+    FILE *accounts_file;
 
-    count_file = fopen("DB/count.txt", "rt");
-    fscanf(count_file, "%d", &num_of_users);
-    fclose(count_file);
+    accounts_file = fopen("DB/accounts.txt", "at");
+    fprintf(accounts_file, "%s\n", account.username);
+    fprintf(accounts_file, "%s\n", account.password);
 
-    count_file = fopen("DB/count.txt", "wt");
-    fprintf(count_file, "%d", num_of_users + 1);
-
-    sprintf(filename, "DB/%d.txt", num_of_users + 1);
-    
-    new_user_file = fopen(filename, "wt");
-
-    fprintf(new_user_file, "%s\n%s", account.username, account.password);
-
-    fclose(count_file);
-    fclose(new_user_file);
+    fclose(accounts_file);
 }
--- src/test.c
+++ src/test.c
@@ -12,7 +12,12 @@ int main() {
     printf("%s\n", curr_user->username);
     printf("%s\n", curr_user->password);
 
-    createAccount("Jelyn", "1234", user_list);
+    createAccount("JP", "1234", user_list);
+
+    curr_user = curr_user->next; // next user
+
+    printf("%s\n", curr_user->username);
+    printf("%s\n", curr_user->password);
 
     curr_user = curr_user->next; // next user
 
