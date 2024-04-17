4d1c91b9194e33173685d620012ee970bc7f9108
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2024-04-17 14:18:10 +0800

    implemented account_management.c

--- src/account_management.c
+++ src/account_management.c
@@ -1,21 +1,40 @@
+#include <string.h>
 #include "account_management.h"
 #include "structs.h"
 
-void changeUser(User account) {
+void changeUser(User* account) {
     char new_username[MAXLEN];
 
-    // Input new username
     printf("Input new username: ");
     scanf("%s", new_username);
 
-    // Save to structure
-    
+    strcpy(account->username, new_username);
+    printf("Username changed successfully");
 }
 
-void changePass(User account) {
+void changePass(User* account) {
+    char new_password[MAXLEN];
 
+    printf("Input new password: ");
+    scanf("%s", new_password);
+
+    strcpy(account->username, new_password);
+    printf("Password changed successfully");
 }
 
-void deleteAccount(User account) {
+// No need to pass *head, as head will never be deleted
+void deleteAccount(User* account, User* head) {
+    User* prev_node;
 
+    // Find node previous to account node
+    while (1) {
+        if (prev_node->next != account){
+            prev_node = prev_node->next;
+            continue;
+        }
+        break;
+    }
+
+    prev_node->next = account->next;
+    free(account);
 }
