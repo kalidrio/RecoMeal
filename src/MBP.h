616ec47cb371e44263399aea5fd590ca878357aa
makiroll.mark <delacruzmacky39@gmail.com>
2024-05-06 21:16:05 +0800

    edited the MBP.h

--- src/MBP.h
+++ src/MBP.h
@@ -1,15 +1,37 @@
+#define stdlen 50
+
+typedef struct { 
+	int ID;
+	char main_course[stdlen];
+	float Mprice;
+	char dessert[stdlen];
+	float Dprice;
+	char beverage[stdlen];
+	float Bprice;
+	float total_price;
+	float sulitness;
+} meal; 
+
 typedef struct {
-    int ID;
-    char name[50];
-    float price;
-    float rating;
-    float sulit_value;
-} food_item;
+	char name[stdlen];
+	float price;
+	float sulitness;
+} purchase;
 
-// User defined functions
-void read_data(FILE *from_catalogue, food_item* catalogue, int food_num);
-void print_Menu(void);
-void peruse(food_item* catalogue, int* errPtr);
 
-// Initialization functions
 int S_init(void);
+
+void clear_buffer(void);
+
+void read_data(FILE* from_meals, meal* catalogueArr);
+/* inputfile has fixed size of 15, size is therefore unneccessary.
+ * count is included as pointer to be used by a later function.
+ */
+
+void read_history(FILE* from_history, purchase* historyArr);
+
+void print_purchase(purchase* historyArr, int items);
+
+void suggest(meal* catalogueArr, int count, FILE* to_history);	
+void budget_it(meal* catalogueArr, int items, float budget, FILE* to_history);
+void inputMode(FILE* to_history);
