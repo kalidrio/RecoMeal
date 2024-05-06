18db8220309706a4db95211e0db69dcc8829b38c
makiroll.mark <delacruzmacky39@gmail.com>
2024-05-06 21:05:17 +0800

    Update MBP.h

--- src/MBP.h
+++ src/MBP.h
@@ -1,15 +1,1 @@
-typedef struct {
-    int ID;
-    char name[50];
-    float price;
-    float rating;
-    float sulit_value;
-} meal;
 
-// User defined functions
-void read_data(FILE *from_catalogue, meal catalogueArr, int* count);
-void print_modes(void);
-void peruse(food_item* catalogue, int* errPtr);
-
-// Initialization functions
-int S_init(void);
